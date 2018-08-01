fdf273d4630806009e870a2c42d382da4eda3f54
John Sawiris <johnedwardsawiris@gmail.com>
2018-07-31 20:56:05 -0700

    Added templates to the routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,21 @@
 // Import Spark library
 import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
+        // Let Spark look into public folder for additional resources
+        staticFileLocation("/public");
         // Setup a route
-        get("/", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Hello From Afar</h1>" +
-                        "<p>Dear Friend,</p>" +
-                        "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                        "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                        "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                        "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                        "<p>Cheers,</p>" +
-                        "<p>Travel Enthusiast Jane</p>" +
-                        "</body>" +
-                        "</html>"
-            );
+        get("/", (request, response) -> {
+                return new ModelAndView(new HashMap(), "hello.hbs");
+            }, new HandlebarsTemplateEngine());
+
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
